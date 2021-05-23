d494f0afce303ff9ef95f73d923a063b125eb9ef
Jan Kučera <kucera.jan@hotmail.cz>
2021-05-23 21:49:38 +0200

    Added test for Double equal to 0

--- Tests/BasicToolsKitTests/NumberExtensionsTests.swift
+++ Tests/BasicToolsKitTests/NumberExtensionsTests.swift
@@ -14,6 +14,7 @@ class NumberExtensionsTests: XCTestCase {
     let number1: Double = 1.151515
     let number2: Double = 2.7
     let number3: Double = 3
+    let number4: Double = 0
     
     
     func testFormatingDouble() {
@@ -22,10 +23,12 @@ class NumberExtensionsTests: XCTestCase {
         let n1 = number1.toString(localeID: "cs")
         let n2 = number2.toString(localeID: "cs")
         let n3 = number3.toString(localeID: "cs")
+        let n4 = number4.toString(localeID: "cs")
         
         XCTAssertEqual(n1, "1,15")
         XCTAssertEqual(n2, "2,7")
         XCTAssertEqual(n3, "3")
+        XCTAssertEqual(n4, "0")
         
     }
     
