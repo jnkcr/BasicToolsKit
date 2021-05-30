6ee278632283dbdb00b9c84bf32b212d0773ab59
Jan Kučera <kucera.jan@hotmail.cz>
2021-05-30 12:37:05 +0200

    Intern documentation added for Numbers

--- Sources/BasicToolsKit/NumberExtensions.swift
+++ Sources/BasicToolsKit/NumberExtensions.swift
@@ -11,18 +11,24 @@ import Foundation
 extension Double {
     
     public func toString(localeID: String? = nil, fractionDigits: Int = 2) -> String {
-        let formatter = NumberFormatter()
         
+        // Setup of formatter
+        let formatter = NumberFormatter()
         formatter.numberStyle           = .decimal
         formatter.maximumFractionDigits = fractionDigits
+        
+        // Check for locale id
         if let localeString = localeID {
             formatter.locale = Locale(identifier: localeString)
         }
         
+        // Convert value
         let number = NSNumber(value: self)
         let formattedValue = formatter.string(from: number)!
         
+        // Return formatted value
         return formattedValue
+        
     }
     
 }
@@ -31,18 +37,24 @@ extension Double {
 extension Float {
     
     public func toString(localeID: String? = nil, fractionDigits: Int = 2) -> String {
-        let formatter = NumberFormatter()
         
+        // Setup of formatter
+        let formatter = NumberFormatter()
         formatter.numberStyle           = .decimal
         formatter.maximumFractionDigits = fractionDigits
+        
+        // Check for locale id
         if let localeString = localeID {
             formatter.locale = Locale(identifier: localeString)
         }
         
+        // Convert value
         let number = NSNumber(value: self)
         let formattedValue = formatter.string(from: number)!
         
+        // Return formatted value
         return formattedValue
+        
     }
     
 }
